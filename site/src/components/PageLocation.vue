8ca59cc5d29083760f0e7eb4936942d6c67d4a95
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-06-12 18:01:57 -0300

    Create Page Location

---
+++ site/src/components/PageLocation.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="page-location-container">
+    <div id="page-location">
+      <div v-for="page in location" :key="page.id">
+        <!-- {{page.name + page.id !== location.length - 1 ? '>' : ''}} -->
+        {{page.name + `${page.id !== location.length - 1 ? ' >' : ''}`}}&nbsp;
+      </div>
+    </div>
+  </div>  
+</template>
+
+
+<script>
+export default {
+  name: 'PageLocation',
+  props: {
+    location: {
+      default: []
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+  #page-location {
+    display: flex;
+    margin-left: 3%;
+  }
+  #page-location div {
+    color: var(--txt-terciary-color);
+  }
+</style>
