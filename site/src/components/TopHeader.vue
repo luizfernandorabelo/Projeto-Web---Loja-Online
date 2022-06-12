c87e1bb0dcd048ae14b4b1598035fb70724f2d3f
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-06-12 12:45:14 -0300

    Create Top Header

---
+++ site/src/components/TopHeader.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="top-header-container">
+    <h1>Azulão Petshop</h1>
+    <img src="../assets/logo.png" alt="Logo">
+    <ul id="icon-list">
+      <li><a href="#"><i class="fa fa-shopping-cart"></i></a></li>
+      <li><a href="#"><i class="fa fa-solid fa-user"></i>Entrar</a></li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'TopHeader',
+}
+</script>
+
+
+<style scoped>
+  #top-header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    height: 50px;
+    padding: 0 40px 0 30px;
+    margin: 10px 0 5px 0;
+    color: var(--txt-secondary-color);
+  }
+  h1 {
+    font-size: 1.3rem;
+    font-weight: 900;
+    margin-left: 5px;
+  }
+  #icon-list {
+    list-style-type: none;
+    display: flex;
+    width: 100px;
+    justify-content: space-between;
+  }
+  #icon-list li:last-child i {
+    margin: 0 5px;
+  }
+</style>
