ec9d40be5c0ded66fe5676fbcffcc761d81de93a
Gustavo Lelli <gustavo.lelli@usp.br>
2022-06-21 02:52:57 -0300

    Add new view

---
+++ site/src/views/ManageProducts.vue
@@ -0,0 +1,91 @@
+<template>
+  <div id="manage-products-container">
+    <PageLocation :location="location"/>
+    <h2>{{h2Content}}</h2>
+    <h3>{{h3Content}}</h3>
+    <ul id="products">
+      <li v-for="product in products" :key="product.id">
+        <MinManageProduct :productName="product.name" :productPrice="product.price" :productImgUrl="product.imgUrl"/>
+      </li>
+    </ul>
+  </div>
+  <router-link to="/manageProducts"><button id="add-new-product-btn" style="color: white">+</button></router-link>
+</template>
+
+
+<script>
+import PageLocation from '../components/PageLocation.vue'
+import MinManageProduct from '../components/MinManageProduct.vue'
+export default {
+  name: 'ManageProducts',
+  components: {
+    PageLocation,
+    MinManageProduct,
+  },
+  props: {
+    h2Content: {
+      type: String,
+      default: '',
+    },
+    h3Content: {
+      type: String,
+      default: '',
+    },
+    products: {
+      default: [
+        { id: 0, name: 'Ração de Teste 1', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 1, name: 'Ração de Teste 2', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 2, name: 'Ração de Teste 3', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 3, name: 'Ração de Teste 4', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 4, name: 'Ração de Teste 5', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 5, name: 'Ração de Teste 6', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 6, name: 'Ração de Teste 7', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 7, name: 'Ração de Teste 8', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 8, name: 'Ração de Teste 9', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+        { id: 9, name: 'Ração de Teste 10', price: 99.99, imgUrl: 'https://lojaludica.com.br/media/catalog/product/cache/1/image/800x/9df78eab33525d08d6e5fb8d27136e95/p/r/produto-teste_1.jpg'},
+      ]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+h2, h3 {
+  text-align: center;
+}
+
+h2 {
+  font-size: 1.1rem;
+  font-weight: 600;
+  margin-bottom: 5px;
+  color: var(--darker-blue-color);
+}
+
+h3 {
+  font-weight: 500;
+  margin-bottom: 20px;
+}
+
+#products {
+  display: flex;
+  justify-content: center;
+  text-align: center;
+  margin: 20px;
+  flex-wrap: wrap;
+  list-style-type: none;
+}
+
+#add-new-product-btn {
+  height: 125px;
+  width: 125px;
+  border-radius: 50%;
+  border: 1px var(--txt-secondary-color);
+  background: var(--txt-secondary-color);
+  font-size: 6rem;
+  font-family: Roboto;
+  position: absolute;
+    bottom: -40px;
+    right: 40px;
+}
+</style>
