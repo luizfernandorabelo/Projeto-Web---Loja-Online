9ccb80d366a640f73b324d2438da641fb7b299c6
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-06-30 22:51:24 -0300

    Make Description have edit option

--- site/src/components/Description.vue
+++ site/src/components/Description.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="description-container">
     <h3>Descrição</h3>
-    <p>{{ text }}</p>
+    <p :contenteditable="editable" @input="onInput">{{ text }}</p>
   </div>
 </template>
 
@@ -11,6 +11,14 @@ export default {
   name: 'Description',
   props: {
     text: String,
+    editable: {
+      default: false,
+    },
+  },
+  methods: {
+    onInput(e) {
+      this.$emit('edit', e.target.innerText);
+    },
   },
 };
 </script>
@@ -32,5 +40,11 @@ h3 {
 p {
   margin-left: 3%;
   margin-right: 3%;
+}
+
+[contenteditable='true'] {
+  border-radius: 5px;
+  outline: 1px solid var(--bg-terciary-color);
+  padding: 10px;
 }
 </style>
