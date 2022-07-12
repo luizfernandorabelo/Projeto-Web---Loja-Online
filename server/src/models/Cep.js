073603e18b6668fcdd4e4df43b2c10f2be0f3a18
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-07-12 12:22:16 -0300

    Update cep schema

--- server/src/models/Cep.js
+++ server/src/models/Cep.js
@@ -3,6 +3,10 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const cepSchema = new Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
   fee: {
     type: Number,
     required: true,
