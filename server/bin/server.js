4b554b97da190b48e4194f05ab49cb5212386965
Luiz Fernando Rabelo <luizfernando2002@hotmail.com>
2022-07-12 13:16:16 -0300

    Create server

--- server/bin/server.js
+++ server/bin/server.js
@@ -1,13 +1,26 @@
+const app = require('../src/app');
+const http = require('http');
 const mongoose = require('mongoose');
-const User = require('../src/models/User');
 
-// Database connection (100% not safe):
-mongoose.connect(
-  `mongodb+srv://azulao:azulao123@cluster0.bspyp.mongodb.net/?retryWrites=true&w=majority`,
-  () => {
-    console.log('connected');
-  },
-  (err) => {
-    console.error(err);
-  }
-);
+const PORT = 3000;
+app.set('port', PORT);
+
+const server = http.createServer(app);
+
+const URI =
+  'mongodb+srv://azulao:azulao123@cluster0.bspyp.mongodb.net/?retryWrites=true&w=majority';
+
+// Database connection (100% unsafe):
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error('Error connecting to database.'));
+db.once('open', () => {
+  console.log('Connected to database.\nRunning on port ' + PORT);
+  server.listen(PORT);
+});
